0c844c7be14cc9a9ac8e64b59df984fc56848f8a
richard-muir <richard.f.muir@gmail.com>
2024-12-18 18:46:38 +0200

    Remove recruiter from app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, session, request, flash
 from dotenv import load_dotenv
 
-from recruiter import recruiter_bp
 from candidate import candidate_bp
 from agents import AgentStore
 from utils import protect_routes
@@ -22,10 +21,8 @@ PASSWORD = os.getenv("FLASK_LOGIN_PASSWORD")
 
 # Pass agent_store to Blueprints
 candidate_bp.agent_store = agent_store
-recruiter_bp.agent_store = agent_store
 
 # Register Blueprints
-app.register_blueprint(recruiter_bp, url_prefix='/recruiter')
 app.register_blueprint(candidate_bp, url_prefix='/candidate')
 protect_routes(app)
 
