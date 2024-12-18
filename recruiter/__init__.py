5777d6db8947f8fbebba14c85ff0c4e1a0369850
richard-muir <richard.f.muir@gmail.com>
2024-12-18 18:44:25 +0200

    remove recruiter module

--- recruiter/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-# Define the Blueprint
-recruiter_bp = Blueprint('recruiter', __name__, template_folder='templates', static_folder='static')
-
-# Add a placeholder for the agent store
-recruiter_bp.agent_store = None
-
-from . import views  # Import routes
